8724f0600577f0ce8101b46a7e4ea46bb3253b3b
rawwr <akshayace0302@gmail.com>
2022-07-20 03:07:08 +0530

    removing a todo

--- react-plus-typescript/src/App.tsx
+++ react-plus-typescript/src/App.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
-import NewTodo from './components/NewTodo';
-import Todos from './components/todos';
-import Modal from './models/model';
+import { useState } from "react";
+import NewTodo from "./components/NewTodo";
+import Todos from "./components/todos";
+import Modal from "./models/model";
 
 function App() {
   const [todos, setTodos] = useState<Modal[]>([]);
@@ -12,14 +12,20 @@ function App() {
     setTodos((prevTodos) => [...prevTodos, newTodo]);
 
     //setTodos((prevTodos) => {
-      // return (prevTodos.concat(newTodo));
+    // return (prevTodos.concat(newTodo));
     // });
-  }
+  };
+
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
 
   return (
     <div>
-    <NewTodo onAddToDo={addTodoHandler}/>
-    <Todos items={todos}/>
+      <NewTodo onAddToDo={addTodoHandler} />
+      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
     </div>
   );
 }
--- react-plus-typescript/src/components/todos.tsx
+++ react-plus-typescript/src/components/todos.tsx
@@ -1,13 +1,23 @@
-import Modal from '../models/model';
-import TodoItem from './TodoItem';
+import Modal from "../models/model";
+import TodoItem from "./TodoItem";
+import classes from "./todos.module.css";
 
-const Todos: React.FC<{items: Modal[]}> = (props) => {
-    return (
-        <ul>
-           {props.items.map((item, index) => <TodoItem key={item.id} text={item.text} />)}
-        </ul>
-    )
-}
+const Todos: React.FC<{
+  items: Modal[];
+  onRemoveTodo: (id: string) => void;
+}> = (props) => {
+  return (
+    <ul className={classes.todos}>
+      {props.items.map((item, index) => (
+        <TodoItem
+          key={item.id}
+          text={item.text}
+          onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}
+        />
+      ))}
+    </ul>
+  );
+};
 export default Todos;
 
 //props can be made optional by ?
