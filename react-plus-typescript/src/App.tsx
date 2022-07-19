eb854cfca609a5b70629fcb5baa44542d33219e7
rawwr <akshayace0302@gmail.com>
2022-07-20 01:49:40 +0530

    managing state and ts

--- react-plus-typescript/src/App.tsx
+++ react-plus-typescript/src/App.tsx
@@ -1,15 +1,26 @@
+import { useState } from 'react';
+import NewTodo from './components/NewTodo';
 import Todos from './components/todos';
 import Modal from './models/model';
 
 function App() {
-  const todos = [
-    new Modal('Learn React'),
-    new Modal('Learn TypeScript'),
-  ]
+  const [todos, setTodos] = useState<Modal[]>([]);
   // console.log(todos);
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Modal(todoText);
+
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
+
+    //setTodos((prevTodos) => {
+      // return (prevTodos.concat(newTodo));
+    // });
+  }
 
   return (
+    <div>
+    <NewTodo onAddToDo={addTodoHandler}/>
     <Todos items={todos}/>
+    </div>
   );
 }
 
