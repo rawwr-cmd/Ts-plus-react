02b52b297be529bb79da1ec2e48b88f03e7b2d2e
rawwr <akshayace0302@gmail.com>
2022-07-20 00:37:21 +0530

    b&b of ts

--- react-plus-typescript/src/components/todos.tsx
+++ react-plus-typescript/src/components/todos.tsx
@@ -1,12 +1,13 @@
-import Modal from '../models/model'
+import Modal from '../models/model';
+import TodoItem from './TodoItem';
+
 const Todos: React.FC<{items: Modal[]}> = (props) => {
     return (
         <ul>
-           {props.items.map((item, index) => <li key={index}>{item.text}</li>)}
+           {props.items.map((item, index) => <TodoItem key={item.id} text={item.text} />)}
         </ul>
     )
 }
 export default Todos;
-
 
 //props can be made optional by ?
